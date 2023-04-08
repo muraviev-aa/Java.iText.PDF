0334cd119f5def1f9a943fae923f045a70143e9a
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-04-08 07:05:29 +0300

    Added changes to gitignore, pom, Main.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        String path = "E:/ITMO/Java_new/test.pdf";
+        /**
+         * Установка шрифта
+         */
+        String fontFilename = "./src/main/resources/arial.ttf";
+        PdfFont font = PdfFontFactory.createFont(fontFilename, PdfEncodings.IDENTITY_H);
+
+        String paraText = "Старт!";
+        Paragraph paragraph = new Paragraph(paraText);
+        PdfWriter pdfWriter = new PdfWriter(path);
+
+        PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+        pdfDocument.addNewPage();
+
+        Document document = new Document(pdfDocument);
+        /**
+         * Назначение шрифта
+         */
+        document.setFont(font);
+        document.add(paragraph);
+
+        document.close();
     }
 }
