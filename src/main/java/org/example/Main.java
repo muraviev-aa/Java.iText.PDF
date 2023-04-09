917d27c00a8fed0c67053abd76b2c2098c0e6f68
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-04-09 07:36:46 +0300

    Added document.add(image).

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,29 @@
 package org.example;
 
 import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        /**
+         * Добавление картинки в pdf
+         */
+        String imgSrc = "images/login.png";
+        ImageData data = ImageDataFactory.create(imgSrc);
+        Image image1 = new Image(data);
+        /**
+         * Путь до файла pdf
+         */
         String path = "E:/ITMO/Java_new/test.pdf";
         /**
          * Установка шрифта
@@ -32,6 +44,7 @@ public class Main {
          */
         document.setFont(font);
         document.add(paragraph);
+        document.add(image1);
 
         document.close();
     }
